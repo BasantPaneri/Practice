1f21854fb810eab9649deb253a80e6b4589ecfa5
BasantPaneri <paneribasant987@gmail.com>
2026-06-03 15:24:30 +0000

    Apply .gitignore rules

---
+++ ArrList.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class ArrList {
+    public static void main(String[] args) {
+        int arr[] = new int[6];
+        for (int i = 0 ; i<arr.length ; i++){
+            arr[i] = i+1 ;
+            System.out.print(arr[i] + " , ");
+        }System.out.println("");
+
+        ArrayList<Integer> obj = new ArrayList<>();
+        for (int i = 0 ; i<arr.length ; i++){
+            obj.add(arr[i]); 
+        }
+
+        head 
+        
+        // LinkL Linkobj = new LinkL();
+        for (int i = 0 ; i < obj.size() ; i++){
+            LinkL Linkobj = new LinkL(obj.get(i));          
+        }
+
+        
+        LinkL obj1 = new LinkL(1); 
+        LinkL obj2 = new LinkL(2); 
+        LinkL obj3 = new LinkL(3); 
+        LinkL obj4 = new LinkL(4); 
+        LinkL obj5 = new LinkL(5); 
+        LinkL obj6 = new LinkL(6); 
+
+        obj1.setNext(obj2);
+        obj2.setNext(obj3);
+        obj3.setNext(obj4);
+        obj4.setNext(obj5);
+        obj5.setNext(obj6);
+
+
+        LinkL.printMain(obj1);
+        System.out.println("");
+    }
+}
